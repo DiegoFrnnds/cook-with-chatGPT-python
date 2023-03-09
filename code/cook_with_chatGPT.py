195a9a17f47dd4c85a9324455e5bc7be3936e214
Diego Martinez <martinezdiego906@gmail.com>
2023-03-09 17:24:24 -0300

    added script code

---
+++ code/cook_with_chatGPT.py
@@ -0,0 +1,25 @@
+# needs PyGithub
+from github import Github
+
+token = "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"
+g = Github(token)
+
+repo = g.get_repo("DiegoFrnnds/cook-with-chatGPT-python")
+
+commit_messages = ["Corrigindo um bug", "Adicionando uma nova funcionalidade",
+                   "Atualizando a documentação", "Refatorando o código",
+                   "Implementando um teste automatizado",
+                   "Melhorando a performance"]
+
+import random
+
+num_commits = random.randint(1, 5)
+
+for i in range(num_commits):
+    message = random.choice(commit_messages)
+    try:
+      contents = repo.get_contents("empty_file")
+      repo.update_file(contents.path, message, message, contents.sha)
+    except:
+      repo.create_file("empty_file", message, "")
+
